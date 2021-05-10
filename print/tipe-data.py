41f9b822a97041e749a7c2aeffcf4d7424ec2eb6
fauzialfanatabanie <fauzi.tekset@gmail.com>
2021-05-10 10:16:49 +0800

    Create tipe-data.py

---
+++ print/tipe-data.py
@@ -0,0 +1,59 @@
+#cara melihat tipe data
+x = "Hallo Fauzi"
+print(x)
+print(type(x))
+
+#contoh contoh tipe data
+a = 20
+print(a)
+print(type(a))
+
+b = 20.6
+print(b)
+print(type(b))
+
+c = 1j
+print(c)
+print(type(c))
+
+d = ["saya","kamu","dia"]
+print(d)
+print(type(d))
+
+e = ("manis","asam","asin")
+print(e)
+print(type(e))
+
+f = range(6)
+print(f)
+print(type(f))
+
+g = {"nama" : "Fauzi", "umur" : 30}
+print(g)
+print(type(g))
+
+h = {"apel","jeruk","jambu"}
+print(h)
+print(type(h))
+
+i = frozenset({"apel", "pisang", "anggur"})
+print(i)
+print(type(i))
+
+j = True
+print(j)
+print(type(j))
+
+k = b"Hellosayangkuuuu"
+print(k)
+print(type(k))
+
+print("======")
+l = bytearray(5)
+print(l)
+print(type(l))
+
+print("======")
+m = memoryview(bytes(1))
+print(m)
+print(type(m))
